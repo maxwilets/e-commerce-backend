a253a774524d63871e394f823032e8321191db3f
Tom Chestnut <tchestnut85@gmail.com>
2020-10-30 22:35:50 -0400

    added columns for the category, product, tag and product-tag models

--- models/Tag.js
+++ models/Tag.js
@@ -2,11 +2,19 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
