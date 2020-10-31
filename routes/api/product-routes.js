18bbb5b3af254aad60af17931d413fc463ed5cc2
Tom Chestnut <tchestnut85@gmail.com>
2020-10-31 02:13:28 -0400

    added a get route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll(
+    {
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name']
+        },
+        {
+          model: Tag,
+          attributes: ['tag_name']
+        }
+      ]
+    }
+  )
+    .then(productData => res.json(productData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
