ba2a9ca41dcd2935fa44446ee19e0081795b3e6d
Tom Chestnut <tchestnut85@gmail.com>
2020-10-31 01:17:05 -0400

    added a get route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,20 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
   // be sure to include its associated Products
+  Category.findAll(
+    {
+      include: {
+        model: Product,
+        attributes: ['category_id']
+      }
+    }
+  )
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
